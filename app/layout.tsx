2086092d1fe9b1cb5a8b2661d9e1ab90dccf1e5a
Rahul Rajani <RahulRajani2006@gmail.com>
2026-04-16 17:14:10 +0100

    Will it index?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import Nav from '@/components/Nav'
+
 
 export const metadata: Metadata = {
   title: "F1 ELO",
@@ -14,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,300;0,400;0,600;0,700;0,900;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body style={{ margin: 0, padding: 0, background: '#0d0d10' }}>
+        <Nav />
         {children}
         <Analytics />
       </body>
